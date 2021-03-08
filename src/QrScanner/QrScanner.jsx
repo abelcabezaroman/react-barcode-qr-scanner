098319b2866093f8e0b29a907d3ae0b05b19d0be
acabezar <contacto@abelcabezaroman.com>
2021-03-08 19:39:06 +0100

    first commit

---
+++ src/QrScanner/QrScanner.jsx
@@ -0,0 +1,37 @@
+import Quagga from 'quagga';
+import { useEffect } from "react"; // ES6
+export function BarcodeScanner () {
+    const initBarcode = () => {
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                target: document.querySelector('.qr')    // Or '#yourElement' (optional)
+            },
+            decoder: {
+                readers: ["code_128_reader", "ean_reader",
+                    "ean_8_reader",
+                    "code_39_reader",
+                    "code_39_vin_reader",
+                    "codabar_reader",
+                    "upc_reader",
+                    "upc_e_reader",
+                    "i2of5_reader",
+                    "2of5_reader",
+                    "code_93_reader",]
+            }
+        }, function (err) {
+            if (err) {
+                console.log(err);
+                return
+            }
+            console.log("Initialization finished. Ready to start");
+            Quagga.start();
+        });
+        Quagga.onDetected((code) => console.log(code))
+
+    }
+    useEffect(initBarcode, [])
+    return (<div className="qr"></div>
+    )
+}
